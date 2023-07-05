833cf66f8f067b7ba6785c38ca7a9a2511b9c173
Reetik Basniwal <reetikbasniwal@gmail.com>
2023-07-05 11:07:36 +0530

    removed excess comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import Album from './Album'
-// import { useState } from 'react';
-// import Update from './Update';
 import { useNavigate } from 'react-router-dom';
 
 
 export default function Home(props) {
   
-  // const [editMode, setEditMode] = useState(false);
   const navigate = useNavigate();
 
 
@@ -20,52 +17,23 @@ export default function Home(props) {
     <div className='container my-3'>
       {console.log('home')}
       <h1 className='mb-5'>Albums</h1>
-          <div className="row">
-          {
-            // RENDERING EVERY ALBUM
-            props.data.map((item) => {
-              return  <div className="col-md-4 mb-5"  key={item.id}>
-                        <Album 
-                          title={item.title} 
-                          id={item.id} userId={item.userId}
-                          onDelete={props.onDelete}
-                          onEdit={() => handleEdit(item)}
-                          currAlbum={item} 
-                        />
-                      </div>
-              }
-            )
-          }
-          </div> 
-        </div>
-      // {/* { editMode ? (<Update 
-      //                 Cancel={handleCancel} 
-      //                 onUpdate={props.onUpdate}
-      //             />) : 
-      //   (
-      //   <>
-      //     <h1 className='mb-5'>Albums</h1>
-      //     <div className="row">
-      //     {
-      //       // RENDERING EVERY ALBUM
-      //       props.data.map((item) => {
-      //         return  <div className="col-md-4 mb-5"  key={item.id}>
-      //                   <Album 
-      //                     title={item.title} 
-      //                     id={item.id} userId={item.userId}
-      //                     onDelete={props.onDelete}
-      //                     onEdit={() => handleEdit(item)}
-      //                     currAlbum={item} 
-      //                   />
-      //                 </div>
-      //         }
-      //       )
-      //     }
-      //     </div> 
-      //    </>
-      //    )
-      // } */}
-     
-    
+        <div className="row">
+        {
+          // RENDERING EVERY ALBUM
+          props.data.map((item) => {
+            return  <div className="col-md-4 mb-5"  key={item.id}>
+                      <Album 
+                        title={item.title} 
+                        id={item.id} userId={item.userId}
+                        onDelete={props.onDelete}
+                        onEdit={() => handleEdit(item)}
+                        currAlbum={item} 
+                      />
+                    </div>
+            }
+          )
+        }
+        </div> 
+    </div>
   )
 }
